57626b508156d36ff3663c7c9d44d7ada561bd15
Isy <isy1997@freenet.de>
2024-01-30 00:13:21 +0100

    Create User Login Form Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,11 +17,13 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 // Component
 import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
+import { UserLoginFormComponent } from './user-login-form/user-login-form.component';
 
 @NgModule({
   declarations: [
     AppComponent, 
-    UserRegistrationFormComponent
+    UserRegistrationFormComponent,
+    UserLoginFormComponent
   ],
   imports: [
     BrowserModule,
