9918f76e669c294c055d03514320e53c92f2a865
Isy <isy1997@freenet.de>
2024-01-30 13:38:37 +0100

    Add Welcome Page component

--- src/app/user-login-form/user-login-form.component.ts
+++ src/app/user-login-form/user-login-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { Router } from '@angular/router';
 
 // Import to close the dialog on success
 import { MatDialogRef } from '@angular/material/dialog';
@@ -21,7 +22,9 @@ export class UserLoginFormComponent implements OnInit{
   constructor(
     public fetchApiData: FetchApiDataService ,
     public dialogRef: MatDialogRef<UserLoginFormComponent>,
-    public snackBar: MatSnackBar) { }
+    public snackBar: MatSnackBar,
+    private router: Router
+  ) { }
   
   ngOnInit(): void {
   }
@@ -37,6 +40,7 @@ export class UserLoginFormComponent implements OnInit{
       this.snackBar.open('User login successful', 'OK', {
         duration: 2000
       });
+      this.router.navigate(['movies'])
     }, (result) => {
       console.log(result);
       this.snackBar.open('User login failed', 'OK', {
--- src/app/welcome-page/welcome-page.component.ts
+++ src/app/welcome-page/welcome-page.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserLoginFormComponent } from '../user-login-form/user-login-form.component';
+import { UserRegistrationFormComponent } from '../user-registration-form/user-registration-form.component';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-welcome-page',
-  standalone: true,
-  imports: [],
   templateUrl: './welcome-page.component.html',
-  styleUrl: './welcome-page.component.scss'
+  styleUrls: ['./welcome-page.component.scss']
 })
-export class WelcomePageComponent {
+export class WelcomePageComponent implements OnInit {
+  constructor(public dialog: MatDialog) {}
+  ngOnInit(): void {}
+
+  openUserRegistrationDialog(): void {
+    this.dialog.open(UserRegistrationFormComponent, {
+      // Assign dialog width
+      width: '280px'
+    });
+  }
+
+  //Function that will open the dialog when the login button is clicked
+  openUserLoginDialog(): void {
+    this.dialog.open(UserLoginFormComponent, {
+      // Assign dialog width
+      width: '280px'
+    });
+  }
 
 }
